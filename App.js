517d6e39c57159caf0a26468e5fdfc1b7809656a
jose <rodolfojose@live.com>
2018-12-18 20:43:11 -0600

    Revert "arreglos"
    This reverts commit edd794d36f17ecaca60ac5365343c350eef83e37.

--- App.js
+++ App.js
@@ -3,68 +3,153 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  TextInput,
   View,
+  FlatList,
+  AsyncStorage,
   Button,
-} from 'react-native';
+  TextInput,
+  Keyboard,
+  Platform
+} from "react-native";
+const isAndroid = Platform.OS == "android";
+const viewPadding = 10;
 
 export default class App extends React.Component {
 
+  state = {
+    tasks: [],
+    text: ""
+  };
+
+  changeTextHandler = text => {
+    this.setState({ text: text });
+  };
+
+  addTask = () => {
+    let notEmpty = this.state.text.trim().length > 0;
+    if (notEmpty) {
+      this.setState(
+        prevState => {
+          let { tasks, text } = prevState;
+          return {
+            tasks: tasks.concat({ key: tasks.length, text: text }),
+            text: ""
+          };
+        },
+        () => Tasks.save(this.state.tasks)
+      );
+    }
+  };
+
+  componentDidMount() {
+    Keyboard.addListener(
+      isAndroid ? "keyboardDidShow" : "keyboardWillShow",
+      e => this.setState({ viewPadding: e.endCoordinates.height + viewPadding })
+    );
+
+    Keyboard.addListener(
+      isAndroid ? "keyboardDidHide" : "keyboardWillHide",
+      () => this.setState({ viewPadding: viewPadding })
+    );
+
+    Tasks.all(tasks => this.setState({ tasks: tasks || [] }));
+  }
 
   render() {
+    return (
+      <View
+        style={[styles.container, { paddingBottom: this.state.viewPadding }]}
+      >
 
-    return (  
-      <View style={styles.container}>
-        <Text style={styles.description}>
-          Job Search
-        </Text>
-        <View style={styles.flowRight}>
-        <TextInput
-          underlineColorAndroid={'transparent'}
-          style={styles.searchInput}
-          placeholder='Search'/>
+        <Text type="title1">TodoApp</Text>
+          <TextInput
+          style={styles.textInput}
+          onChangeText={this.changeTextHandler}
+          onSubmitEditing={()=>{this.addTask(this.state.text)}}
+          value={this.state.text}
+          placeholder="Write a ToDo"
+          returnKeyType="done"
+          returnKeyLabel="done"
+        /> 
+    <View style={styles.buttonContainer}>
+        <Button title="+" onPress={ () => this.addTask(this.state.text) }/>
+        </View>
+        <FlatList
+          style={styles.list}
+          data={this.state.tasks}
+          renderItem={({ item }) =>
+            <View>
+              <View style={styles.listItemCont}>
+                <Text style={styles.listItem}>
+                  {item.text}
+                </Text>
+              </View>
+              <View style={styles.hr} />
+            </View>}
+        />
       </View>
-      <Button style={styles.button}
-        
-        title="Search"
-      />
-      </View>
-    )
+    );
   }
 }
 
-
-
+let Tasks = {
+  convertToArrayOfObject(tasks, callback) {
+    return callback(
+      tasks ? tasks.split("||").map((task, i) => ({ key: i, text: task })) : []
+    );
+  },
+  convertToStringWithSeparators(tasks) {
+    return tasks.map(task => task.text).join("||");
+  },
+  all(callback) {
+    return AsyncStorage.getItem("TASKS", (err, tasks) =>
+      this.convertToArrayOfObject(tasks, callback)
+    );
+  },
+  save(tasks) {
+    AsyncStorage.setItem("TASKS", this.convertToStringWithSeparators(tasks));
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    padding: viewPadding,
+    paddingTop: 50
   },
-  description: {
-    fontSize: 18,
-    color: '#656565',
-    marginTop: 50,
+  list: {
+    width: "95%"
   },
-  flowRight: {
-    flexDirection: 'row',
-    alignSelf: 'stretch',
+  listItem: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontSize: 15
   },
-  searchInput: {
-    height: 36,
-    padding: 4,
-    marginRight: 5,
-    flexGrow: 1,
-    fontSize: 18,
-    borderWidth: 1,
-    borderRadius: 8,
+  buttonContainer: {
+    position: 'absolute',
+    top: 120,
+    left: 320,
+    width: "20%",
+    margin:2
   },
-  button: {
-    margin: 20,
-    width: 400,
+  hr: {
+    height: 1,
+    backgroundColor: "gray"
   },
-
+  listItemCont: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    height: 150,
+    paddingRight: 20,
+    paddingLeft: 10,
+    borderColor: "gray",
+    borderWidth: isAndroid ? 0 : 1,
+    width: "100%"
+  }
 });
-
